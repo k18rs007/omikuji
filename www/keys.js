42cd63c95669df29bb7d5eaa472e496a98c81cfa
k18rs007 <k18rs007@st.kyusan-u.ac.jp>
2019-12-16 13:40:26 +0900

    Project's Initial Commit

---
+++ www/keys.js
@@ -0,0 +1,4 @@
+// This is a JavaScript file
+
+ var appKey= "62ad265bb2dcdeefcc300180d43e88ad7dd1745ebeb997503ccdcd4a27fdb4c1";
+var clientKey = "1ee4b42f6246e8fd10929f4ae2fa92381031232418b13a0b17d8e5c4946fa8ee";
